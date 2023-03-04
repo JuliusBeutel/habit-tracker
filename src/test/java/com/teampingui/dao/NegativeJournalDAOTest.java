ee93ff9f07b4b2f58e6068a44f1801c55cc39ead
jb266 <jb266@hdm-stuttgart.de>
2023-03-04 12:06:15 +0100

    upload project

---
+++ src/test/java/com/teampingui/dao/NegativeJournalDAOTest.java
@@ -0,0 +1,35 @@
+package com.teampingui.dao;
+
+import com.teampingui.exceptions.JournalDaoException;
+import com.teampingui.models.JournalEntry;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class NegativeJournalDAOTest {
+    JournalDAO journalDAO = new JournalDAO();
+    JournalEntry journalEntry = new JournalEntry("05.05.1999", null);
+
+    @Test
+    public void testInsertJournalEntry() {
+        try {
+            journalDAO.insert(journalEntry);
+        } catch (JournalDaoException e) {
+            System.out.println("Right exception thrown (JournalDaoException)");
+        } catch (Exception e) {
+            fail("Unexpected exception call in testInsertJournalEntry. Expected JournalDaoException");
+        }
+    }
+
+    //This test is for checking that an exception gets thrown, when the setID method in JournalEntryItem gets passed
+    //a negative value
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetIdOfJournalEntry() {
+        try {
+            JournalEntry journalEntry = new JournalEntry("20.04.2020", "This text came from testSetIdOfJournalEntry");
+            journalEntry.setID(-1);
+        } catch (IllegalArgumentException i) {
+            throw new IllegalArgumentException(i);
+        }
+    }
+}
